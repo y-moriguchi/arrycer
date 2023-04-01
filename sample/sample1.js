854045e91aa2c18946ca9d5c4edf0e4e880a94b7
y-moriguchi <daikanyamayuiichi@gmail.com>
2023-04-02 08:01:29 +0900

    add Arrycer samples

---
+++ sample/sample1.js
@@ -0,0 +1,102 @@
+/**
+ * Arrycer
+ *
+ * Copyright (c) 2023 Yuichiro MORIGUCHI
+ *
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ **/
+/*
+ * Arrycer samples
+ */
+
+const A = Arrycer();
+
+// compute prime numbers
+function prime(n) {
+    const r = A.iterate(x => x + 1, 2, n - 1);
+
+    return A.replicateAxis(r, A.member(r, A.outer(r, r, (x, y) => x * y)).map(x => x ? 0 : 1), 0);
+}
+
+// compute prime numbers (another version)
+function primeto(n) {
+    const s = Math.sqrt(n);
+    let m = A.reshape(A.take([1], [n], 0).map(x => x ? 0 : 1), n, 1);
+
+    for(let np = 2; np <= s; np += A.lastIndexOfArray(A.subarray(m, [1, null]), 1) + 1) {
+        m = A.reshape(m, Math.ceil(n / np), np);
+        A.set(m, [A.iterate(x => x + 1, 1, m.length - 1), np - 1], 0);
+    }
+    return A.replicateAxis(A.iterate(x => x + 1, 1, n), A.reshape(m, n), 0);
+}
+
+// plot level by characters
+const plot1 = (matrix, level, chars) =>
+    A.atArray(chars, A.reduceDeep(A.outer(matrix, level, (x, y) => x >= y ? 1 : 0), (accum, x) => accum + x, 100, -1));
+
+// compute Fibonacci sequence
+const fib = nth => A.first(
+    A.drop(
+        A.replicateAxis([{ e: A.reshape([1, 1, 1, 0], 2, 2) }], [nth], 0, 0)
+        .reduce(
+            (accum, x) => ({ e: A.inner(accum.e, x.e, (accum, x) => accum + x, (accum, x) => accum * x) })).e,
+        [0, 1]));
+
+// create Hilbert matrix and compute error
+function hilbert(b) {
+    const h = A.outer(A.iterate(x => x + 1, 1, b.length), A.iterate(x => x + 1, 1, b.length), (x, y) => 1 / (x + y - 1));
+
+    return A.map((x, y) => x - y, b, A.inner(h, A.solveMatrix(h, b), (accum, x) => accum + x, (accum, x) => accum * x));
+}
+
+// Conway's game of life
+function life(p) {
+    return A.inner(
+        [3, 4].map(x => A.map((y, z) => y === z, A.reduceAxis(A.reduceAxis(
+            A.outer([-1, 0, 1].map(x => A.rotateAxis(p, x, 1)), [-1, 0, 1],
+                (x, y) => A.rotateAxis(x, y, 0), 1), (accum, x) => accum + x, 0, 0), (accum, x) => accum + x, 0), [[x]])),
+        [[[1]], p],
+        (x, y) => A.map((z, w) => z || w ? 1 : 0, x, y),
+        (x, y) => A.map((z, w) => z && w ? 1 : 0, x, y), void 0, void 0, 1, 1);
+}
+
+// Conway's game of life (another version)
+function life2(p) {
+    return A.inner(
+        [3, 4].map(x => A.map((y, z) => y === z, A.vectorize(
+            A.outer([-1, 0, 1].map(x => A.rotateAxis(p, x, 1)), [-1, 0, 1],
+                (x, y) => A.rotateAxis(x, y, 0), 1), 2).reduce((accum, x) => A.map((x, y) => x + y, accum, x)), [[x]])),
+        [[[1]], p],
+        (x, y) => A.map((z, w) => z || w ? 1 : 0, x, y),
+        (x, y) => A.map((z, w) => z && w ? 1 : 0, x, y), void 0, void 0, 1, 1);
+}
+
+// solve linear equation by Jacobi method
+function jacobi(a, b) {
+    const d = A.transpose(a, 0, 0);
+    let x = A.reshape([0], b.length);
+
+    for(let i = 0; i < 100; i++) {
+        const xx = A.map((x, y) => x + y,
+            A.map((x, y) => x / y, A.map((x, y) => x - y, b, A.inner(a, x, (accum, x) => accum + x, (accum, x) => accum * x)), d),
+            x);
+
+        if(Math.sqrt(A.map((x, y) => x - y, xx, x).reduce((accum, x) => accum + x * x) / xx.reduce((accum, x) => accum + x * x)) < 1e-5) {
+            x = xx;
+            break;
+        } else {
+            x = xx;
+        }
+    }
+    return x;
+}
+
+// remove HTML tags
+function removeTag(txt) {
+    const t = txt.split("");
+
+    return A.replicateAxis(t, (x => A.map1((x, y) => x || y ? 1 : 0, A.scanAxis(x, (accum, x) => accum !== x ? 1 : 0, 0), x))
+        (A.member(t, ["<", ">"])).map(x => !x ? 1 : 0), 0).join("");
+}
+
